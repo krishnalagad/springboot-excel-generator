0c78ad4ab91df12a5ded2bfde5ea0be1ea56f016
Krishna Lagad <krishnalagad2@gmail.com>
2023-03-05 21:36:16 +0530

    - create springboot project

---
+++ excel-generator/src/test/java/com/excel/excelgenerator/ExcelGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excel.excelgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
